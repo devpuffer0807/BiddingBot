0924e7b1008de61d26696a355e2721e6c484a197
Shola Ayeni <ayenisholah@yahoo.com>
2024-09-05 12:20:34 +0100

    remove wallet from browser storage

--- src/store/wallet.store.ts
+++ src/store/wallet.store.ts
@@ -1,5 +1,4 @@
 import { create } from "zustand";
-import { persist } from "zustand/middleware";
 
 interface Wallet {
   _id: string;
@@ -18,47 +17,32 @@ interface WalletState {
 }
 
 export const useWalletStore = create<WalletState>()(
-  persist(
-    (set, get) => ({
-      wallets: [],
-      addWallet: (wallet) =>
-        set((state) => ({
-          wallets: [
-            ...state.wallets,
-            {
-              _id: wallet.address,
-              name: wallet.name,
-              address: wallet.address,
-              privateKey: wallet.privateKey,
-            },
-          ],
-        })),
-      editWallet: (id, name) =>
-        set((state) => ({
-          wallets: state.wallets.map((wallet) =>
-            wallet._id === id ? { ...wallet, name } : wallet
-          ),
-        })),
-      deleteWallet: (id) =>
-        set((state) => ({
-          wallets: state.wallets.filter((wallet) => wallet._id !== id),
-        })),
-      clearWallets: () => set({ wallets: [] }),
-      getWallet: (id) => get().wallets.find((wallet) => wallet._id === id),
-    }),
-    {
-      name: "wallet",
-      storage: {
-        getItem: (name) => {
-          const str = localStorage.getItem(name);
-          if (!str) return null;
-          return JSON.parse(str);
-        },
-        setItem: (name, value) => {
-          localStorage.setItem(name, JSON.stringify(value));
-        },
-        removeItem: (name) => localStorage.removeItem(name),
-      },
-    }
-  )
+  // Remove the persist middleware
+  (set, get) => ({
+    wallets: [],
+    addWallet: (wallet) =>
+      set((state) => ({
+        wallets: [
+          ...state.wallets,
+          {
+            _id: wallet.address,
+            name: wallet.name,
+            address: wallet.address,
+            privateKey: wallet.privateKey,
+          },
+        ],
+      })),
+    editWallet: (id, name) =>
+      set((state) => ({
+        wallets: state.wallets.map((wallet) =>
+          wallet._id === id ? { ...wallet, name } : wallet
+        ),
+      })),
+    deleteWallet: (id) =>
+      set((state) => ({
+        wallets: state.wallets.filter((wallet) => wallet._id !== id),
+      })),
+    clearWallets: () => set({ wallets: [] }),
+    getWallet: (id) => get().wallets.find((wallet) => wallet._id === id),
+  })
 );
