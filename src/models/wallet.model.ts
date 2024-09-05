f8ba39bbcbcce5bdb5bcb4afac53c140c759f45f
Shola Ayeni <ayenisholah@yahoo.com>
2024-09-05 12:36:29 +0100

    create wallet model

---
+++ src/models/wallet.model.ts
@@ -0,0 +1,25 @@
+import mongoose, { model, Model, Schema } from "mongoose";
+
+interface IWallet extends Document {
+  _id: mongoose.Types.ObjectId;
+  user: mongoose.Types.ObjectId;
+  name: string;
+  address: string;
+  privateKey: string;
+}
+
+const WalletSchema = new Schema<IWallet>(
+  {
+    user: { type: Schema.Types.ObjectId, ref: "User", required: true },
+    name: { type: String, required: true },
+    address: { type: String, required: true },
+    privateKey: { type: String, required: true },
+  },
+  { timestamps: true }
+);
+
+const Wallet =
+  (mongoose.models.Wallet as Model<IWallet>) ||
+  model<IWallet>("Wallet", WalletSchema);
+
+export default Wallet;
