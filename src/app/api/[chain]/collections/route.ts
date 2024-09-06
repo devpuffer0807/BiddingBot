5d43f3402d697cb91c7b3b1677bfc578d4f0e707
Shola Ayeni <ayenisholah@yahoo.com>
2024-09-06 10:34:15 +0100

    remove log for errora

--- src/app/api/[chain]/collections/route.ts
+++ src/app/api/[chain]/collections/route.ts
@@ -39,7 +39,6 @@ export async function GET(
       );
     }
   } catch (error) {
-    console.error("Error:", error);
     return NextResponse.json({ error: "An error occurred" }, { status: 500 });
   }
 }
