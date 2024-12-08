98e351be7ce0004bcdd82aff35c736a96aea31f2
Shola Ayeni <ayenisholah@yahoo.com>
2024-12-08 10:39:09 +0100

    implement bid loop count and timer

--- src/app/api/progress/route.ts
+++ src/app/api/progress/route.ts
@@ -22,10 +22,16 @@ export async function POST(request: NextRequest) {
 
     const { tasks }: { tasks: Task[] } = await request.json();
     const orderCounts: BidStats = {};
+    const loopStat: LoopStat = {};
 
     await Promise.all(
       tasks.map(async ({ selectedMarketplaces, taskId }) => {
         orderCounts[taskId] = { opensea: 0, magiceden: 0, blur: 0 };
+        loopStat[taskId] = {
+          opensea: { count: 0, nextLoop: Date.now() },
+          magiceden: { count: 0, nextLoop: Date.now() },
+          blur: { count: 0, nextLoop: Date.now() },
+        };
 
         await Promise.all(
           selectedMarketplaces.map(async (marketplace: Marketplace) => {
@@ -35,10 +41,23 @@ export async function POST(request: NextRequest) {
               parseInt(count, 10);
           })
         );
+
+        await Promise.all(
+          selectedMarketplaces.map(async (marketplace: Marketplace) => {
+            const loopCountKey = `loop_count:${marketplace.toLowerCase()}:${taskId}`;
+            const nextLoopKey = `next_loop:${marketplace.toLowerCase()}:${taskId}`;
+            const loopCount = (await redis.get(loopCountKey)) ?? "";
+            const nextLoop = (await redis.get(nextLoopKey)) ?? "";
+            loopStat[taskId][marketplace.toLowerCase() as Marketplace].count =
+              parseInt(loopCount, 10);
+            loopStat[taskId][
+              marketplace.toLowerCase() as Marketplace
+            ].nextLoop = parseInt(nextLoop, 10);
+          })
+        );
       })
     );
-
-    return NextResponse.json(orderCounts, { status: 200 });
+    return NextResponse.json({ orderCounts, loopStat }, { status: 200 });
   } catch (error) {
     console.error("Progress API error:", error);
 
@@ -54,5 +73,22 @@ interface BidStats {
     opensea: number;
     magiceden: number;
     blur: number;
+  };
+}
+
+export interface LoopStat {
+  [key: string]: {
+    opensea: {
+      count: number;
+      nextLoop: number;
+    };
+    magiceden: {
+      count: number;
+      nextLoop: number;
+    };
+    blur: {
+      count: number;
+      nextLoop: number;
+    };
   };
 }
